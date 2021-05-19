b049cc6f0e6b80459d9633c97d0f56c6d328f7d0
SR089850 <SR089850@W1968424.northamerica.cerner.net>
2021-05-19 12:46:18 +0530

    Controller Changes

--- demo/src/main/java/com/example/demo/controller/Controller.java
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-	
-	
+
 	@GetMapping("/test")
 	public String showMessage() {
 		return "Hello World from Docker";
+	}
+
+	@GetMapping("/name/{name}")
+	public String showName(@PathVariable("name") String name) {
+		return "My name is " + name;
 	}
 
 }
