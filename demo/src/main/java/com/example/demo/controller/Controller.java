f871940a2970819b466cfe365c5f52ad6f8ce619
SR089850 <SR089850@W1968424.northamerica.cerner.net>
2021-05-20 12:43:18 +0530

    Swagger UI

--- demo/src/main/java/com/example/demo/controller/Controller.java
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -2,7 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.Employee;
+
 
 @RestController
 public class Controller {
@@ -15,6 +19,12 @@ public class Controller {
 	@GetMapping("/name/{name}")
 	public String showName(@PathVariable("name") String name) {
 		return "My name is " + name;
+	}
+	
+	@PostMapping("/addEmployee")
+	public String addEmplyee(@RequestBody Employee e) {
+		
+		return "Employee added with the name "+e.getName();
 	}
 
 }
